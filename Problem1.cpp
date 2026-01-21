9126760cc2feaa369bf8e9e7833bdfa0b5b24ce2
Pratham Shettigar <shettigarpratham6@gmail.com>
2026-01-21 10:53:11 +0530

    Add files via upload

---
+++ Problem1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int unusual_five()
+{
+  int len;
+  string name="Prath";
+  len=name.size();
+    return len;
+}
+int main()
+{
+    int num;
+    num=unusual_five();
+    return 0;
+}
